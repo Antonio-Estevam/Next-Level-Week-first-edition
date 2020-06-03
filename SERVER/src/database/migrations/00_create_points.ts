765465b22eb0680ba5f5992f60a25eb079e6e19f
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2020-06-02 23:57:08 -0300

    create DB -01

---
+++ SERVER/src/database/migrations/00_create_points.ts
@@ -0,0 +1,24 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    //criar tabela 
+
+    return knex.schema.createTable('points', table => {
+         table.increments('').primary();
+         table.string('image').notNullable();
+         table.string('name').notNullable();
+         table.string('email').notNullable();
+         table.string('whatsapp').notNullable();
+         table.decimal('latitude').notNullable();
+         table.decimal('longitude').notNullable();
+         table.string('city').notNullable();
+         table.string('uf',2).notNullable();
+    });
+
+}
+
+export async function down(knex: Knex){
+    //refazer alterações
+
+    return knex.schema.dropTable('points');
+}
