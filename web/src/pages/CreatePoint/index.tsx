57c177f03b440c440aadbf475b9eb08822165cdd
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2020-06-05 20:34:44 -0300

    Create Back End -02

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,199 @@
+import React, {useEffect, useState, ChangeEvent} from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+import axios from 'axios';
+
+import './style.css';
+import logo from '../../assets/logo.svg';
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface IBGEUFResponse {
+    sigla: string;
+}
+interface IBGECityResponse {
+    nome: string;
+}
+/*
+interface UF {
+    name: string;
+}*/
+
+const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, seCities] = useState<string[]>([]);
+
+    const [selectedUf, setselectedUf] = useState("0")
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    },[]);
+
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then(response =>{
+            const ufInitials = response.data.map(uf => uf.sigla);
+            
+            setUfs(ufInitials);
+        });
+    }, []);
+
+    useEffect(() => {
+        if(selectedUf === '0'){
+        return;
+        }else{
+            axios
+            .get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+            .then(response =>{
+                const cityNames = response.data.map(city => city.nome);
+                
+                seCities(cityNames);
+            });
+        }
+    }, [selectedUf]);
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+        const uf = event.target.value;
+
+        setselectedUf(uf);                
+    }    
+
+    return(
+       <div id="page-create-point">
+           <header>
+               <img src={logo} alt="Ecolata"/>
+
+               <Link to="/">
+                 <FiArrowLeft />
+                 Voltar para home
+               </Link>
+           </header>
+
+           <form>
+               <h1>Cadastro do <br /> ponto de coleta </h1>
+
+               <fieldset>
+                   <legend>
+                       <h2>Dados</h2>
+                   </legend>
+
+                   <div className="field">
+                       <label htmlFor="name">Nome da entidade</label>
+                       <input 
+                            type="text"
+                            name="name"
+                            id="name"
+                        />
+                   </div>
+
+                   <div className="field-group">
+
+                   <div className="field">
+                       <label htmlFor="email">E-mail</label>
+                       <input 
+                            type="email"
+                            name="email"
+                            id="email"
+                        />
+                   </div>
+
+                   <div className="field">
+                       <label htmlFor="whatsapp">Whatsapp</label>
+                       <input 
+                            type="text"
+                            name="whatsapp"
+                            id="whatsapp"
+                        />
+                   </div>
+
+                   </div>
+               </fieldset>
+
+               <fieldset>
+                   <legend>
+                       <h2>Endereço</h2>
+                       <span> Selecione o Endereço no mapa </span>
+                   </legend>
+
+                   <Map center={[-23.7048854,-46.5644216]} zoom={15}>
+                   <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+
+                    <Marker position={[-23.7048854,-46.5644216]} />
+                   </Map>    
+
+                   <div className="field-group">  
+                        <div className="field"> 
+                            <label htmlFor="uf"> Estado (UF)</label>
+                            <select 
+                                name="uf" 
+                                id="uf" 
+                                value={selectedUf} 
+                                onChange={handleSelectUf}
+                            >
+                                <option value="0">Selecione o UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+
+                                ))}
+                            </select> 
+
+                        </div>  
+
+                        <div className="field"> 
+                            <label htmlFor="city"> Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione a Cidade</option>
+                                
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+
+                                ))}
+                            </select> 
+
+                        </div>                  
+                   </div>
+
+
+               </fieldset>
+
+               <fieldset>
+                   <legend>
+                       <h2>Ítens de coleta </h2>
+                       <span> Selecione um ou mais itens abaixo </span>
+                   </legend>
+
+                    <ul className="items-grid">
+
+                    {items.map(item => (
+                         <li key={item.id} className="selected">
+                         <img src={item.image_url} alt={item.title}/>
+                    <span>{item.title}</span>
+                     </li>
+                    ))}                                            
+                      
+                    </ul>
+
+               </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta 
+                </button>
+
+           </form>
+       </div> 
+    );
+};
+
+export default CreatePoint;
